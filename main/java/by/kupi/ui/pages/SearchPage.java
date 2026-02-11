dc7b670ec6e6db8b5833f7b5f960751be56387ff
NadezhdaSinkevich <n.sinkevich@rambler.ru>
2026-02-11 19:07:17 +0300

    add search tests

--- main/java/by/kupi/ui/pages/SearchPage.java
+++ main/java/by/kupi/ui/pages/SearchPage.java
@@ -13,11 +13,28 @@ public class SearchPage {
     public By buttonSearchDropdowmPath = By.xpath("//button[@id='form-search-search-prefix']");
     public By errorMessagePath = By.xpath("//span[@class='FindTips__textNoResult']");
 
+    public By imageElementDropdowmPath = By.xpath("//div[@class='BlockItemTips']/a[@class='imgBlockTips']/img[@class='iconItemTips']");
+    public By textElementDropdownPath = By.xpath("//div[@class='infoItemTips']/a[@class='titleTips']");
+    public By priceElementDropdownPath = By.xpath("//div[@class='infoItemTips']/div[@class='itemPriceTips']/span[@class='FirstPrice']");
+    public By categoriesDropdownPath = By.xpath("//div[@class='blockSectionTips']/a");
+
     public String placeholderSearchText = "поиск";
     public String iconSearchHref = "#icon-search";
     public String iconSearchDeleteHref = "#icon-close";
     public String errorMessageText = "К сожалению, ничего не найдено";
-    public String buttonSearchDropdownText = "Поиск по ";
+
+    public String testElementTextForSearch = "Xiaomi Book Pro 16\" OLED 2022 (Core i5-1240P, 16Gb, 512Gb, Intel Iris Xe Graphics) (JYU4468CN)";
+    public String testElementImageForSearch ="https://kupi.by/pics/items/icon_20240327190306286_5787-1.jpg";
+    public String testElementPriceForSearch = "5000 руб.";
+
+    public String testCategoriesForSearch = "Товары для дома";
+    public String testCategoriesHrefForSearch ="https://kupi.by/texnika-dlya-doma/";
+
+    public String testSubcategoriesForSearch = "Кофемашины";
+    public String testSubcategoriesElementTextForSearch = "Кофемашина DeLonghi Dinamica ECAM 350.15.B";
+    public String testSubcategoriesElementImageForSearch ="https://kupi.by/pics/items/icon_20240924180942811_Delonghi-ECAM-350.15.B-1.jpg";
+    public String testSubcategoriesElementPriceForSearch = "1460 руб.";
+
 
     private WebDriver driver;
 
@@ -67,5 +84,25 @@ public class SearchPage {
 
     public void clickButtonSearchDropdowm(){
         Driver.waitAndClick(buttonSearchDropdowmPath);
+    }
+
+    public String getImageElementDropdowmPath(){
+        return Driver.waitAndGetTextByAttribute(imageElementDropdowmPath,"src");
+    }
+
+    public String getTextElementDropdownPath(){
+        return Driver.waitAndGetText(textElementDropdownPath);
+    }
+
+    public String getPriceElementDropdowm(){
+        return Driver.waitAndGetText(priceElementDropdownPath);
+    }
+
+    public String getCategoriesSearchHref(){
+        return Driver.waitAndGetTextByAttribute(categoriesDropdownPath,"href");
+    }
+
+    public String getTextCategoriesSearch() {
+        return Driver.waitAndGetText(categoriesDropdownPath);
     }
 }
